12a76459d66f6666ea478491362ba67e1ca2283d
Emily Z <emilyz@Emilys-MacBook-Pro.local>
2021-03-09 18:45:09 -0800

    Models.py edited

--- mysite/mentalcheck/models.py
+++ mysite/mentalcheck/models.py
@@ -4,7 +4,6 @@ class User(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     age = models.IntegerField()
-    questions = models.ManyToManyField(QuestionMC)
 class QuestionMC(models.Model):
     answerChoices = (
         ('a', 'x'),
@@ -13,10 +12,12 @@ class QuestionMC(models.Model):
     )
     answer = models.CharField(max_length=1, choices=answerChoices)
     date = models.DateField()
+    user = models.ForeignKey(User)
 class QuestionText(models.Model):
     date = models.DateField()
     answer = models.CharField(max_length=60)
+    user = models.ForeignKey(User)
 class Support(models.Model):
     ifSupportNeeded = models.CharField(max_length=30)
-
+    user = models.ForeignKey(User)
 # Create your models here.
